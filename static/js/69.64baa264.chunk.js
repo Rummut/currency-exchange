eeba43df6036c2a14c3b29730db6a620921a93c1
Rummut <123414878+Rummut@users.noreply.github.com>
2023-10-07 09:55:15 +0000

    Deploying to gh-pages from @ Rummut/currency-exchange@a898ecdba86a5091f87e18024e5865fd864f9e23 🚀

---
+++ static/js/69.64baa264.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[69],{69:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("h2",{children:"Rates "})}}}]);
+//# sourceMappingURL=69.64baa264.chunk.js.map
